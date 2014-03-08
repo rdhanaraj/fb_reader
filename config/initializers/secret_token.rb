efcbcdd0d0c39b803fd99c32d994cc86cb0b24e6
Rishi Dhanaraj <rishid97@gmail.com>
2014-03-07 18:30:01 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FbReader::Application.config.secret_key_base = '260ffec1b115b92a03b2689503e087f66f9c4c891783cfdd3f1707af3632748732b8730922c9b3b79d23925d8fdc222ae7007501ad07adfaac04450b2fccaff6'
